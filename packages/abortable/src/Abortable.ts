01848431e51f2dcd151bcb8e4ff438ad6d79efc3
Dean Merchant <deanmerchant@gmail.com>
2018-05-04 22:21:07 +0100

    Dropped dependency on lodash for `abortable`

--- packages/abortable/src/Abortable.ts
+++ packages/abortable/src/Abortable.ts
@@ -1,4 +1,4 @@
-import { forEach, identity, map, reduce } from 'lodash';
+const identity = <T>(t: T): T => t;
 
 export type Abortable<T> = Promise<T> & { abort(): void };
 
@@ -6,15 +6,16 @@ export type Cancelable<T> = Promise<T> & { cancel(): void };
 
 export type AbortableLike<T> = Abortable<T> | Cancelable<T> | Promise<T> | T;
 
-const abort = (abortables: Array<AbortableLike<any>>) =>
-  forEach(abortables, abortable => {
+const abort = (abortables: Array<AbortableLike<any>>): void => {
+  for (const abortable of abortables) {
     if (abortable.abort != null) {
       return abortable.abort();
     }
     if (abortable.cancel != null) {
       return abortable.cancel();
     }
-  });
+  }
+};
 
 export function all<T>(abortables: [AbortableLike<T>]): Abortable<[T]>
 export function all<T1, T2>(abortables: [AbortableLike<T1>, AbortableLike<T2>]): Abortable<[T1, T2]>
@@ -28,12 +29,11 @@ export function all<T1, T2, T3, T4, T5, T6, T7, T8, T9>(abortables: [AbortableLi
 export function all<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>(abortables: [AbortableLike<T1>, AbortableLike<T2> | AbortableLike<T3>, AbortableLike<T4>, AbortableLike<T5>, AbortableLike<T6>, AbortableLike<T7>, AbortableLike<T8>, AbortableLike<T9>, AbortableLike<T10>]): Abortable<[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10]>
 export function all(abortables: Array<any | Abortable<any>>): Abortable<Array<any>> {
   // If the abortable is a superagent request the request wont be sent until it has been then'd
-  const initiatedRequests: Array<Promise<any>> = map(abortables, (abortable): Promise<any> =>
+  const initiatedRequests: Array<Promise<any>> = abortables.map((abortable): Promise<any> =>
     abortable.then ? abortable.then(identity) : Promise.resolve(abortable));
 
   const results: Array<any> = [];
-  const mergedAbortable: Promise<any> = reduce(
-    initiatedRequests,
+  const mergedAbortable: Promise<any> = initiatedRequests.reduce(
     (chainedAbortables, abortable) =>
       chainedAbortables.then(() => abortable).then((result: any) => results.push(result)),
     Promise.resolve(0),
@@ -64,18 +64,20 @@ export function race<T1, T2, T3, T4, T5, T6, T7, T8>(abortables: [AbortableLike<
 export function race<T1, T2, T3, T4, T5, T6, T7, T8, T9>(abortables: [AbortableLike<T1>, AbortableLike<T2> | AbortableLike<T3>, AbortableLike<T4>, AbortableLike<T5>]): Abortable<T1 | T2 | T3 | T4 | T5>
 export function race<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>(abortables: [AbortableLike<T1>, AbortableLike<T2> | AbortableLike<T3>, AbortableLike<T4>, AbortableLike<T5>]): Abortable<T1 | T2 | T3 | T4 | T5>
 export function race(abortables: Array<Abortable<any>>): Abortable<any> {
-  const result = new Promise((resolve, reject) =>
-    forEach(abortables, (abortable) => abortable
-      .then((result) => {
-        abort(abortables);
-        resolve(result);
-      })
-      .catch(reason => {
-        abort(abortables);
-        reject(reason);
-      })
-    )) as Abortable<any>;
+  const result = new Promise((resolve, reject) => {
+    for (const abortable of abortables) {
+      abortable
+        .then((result) => {
+          abort(abortables);
+          resolve(result);
+        })
+        .catch(reason => {
+          abort(abortables);
+          reject(reason);
+        })
+    }
+  }) as Abortable<any>;
 
   result.abort = () => abort(abortables);
-  return result;
+  return result as Abortable<any>;
 }
