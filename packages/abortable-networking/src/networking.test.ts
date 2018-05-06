541ef3f64eeaf8377921d2b3ecc55d0ca6d47ac3
Dean Merchant <deanmerchant@gmail.com>
2018-05-06 15:06:06 +0100

    Drop the pre-publish script, it runs post install (before we have had a chance to bootstrap with lerna.

--- packages/abortable-networking/src/networking.test.ts
+++ packages/abortable-networking/src/networking.test.ts
@@ -1,7 +1,7 @@
 import { fetchJson, from } from './networking';
 
 describe('abortable-networking', () => {
-  it('can be aborted, preventing and onfufuilled handlers from being called', () => {
+  it('can be aborted, preventing any success handlers from being called', () => {
     const successSpy = jest.fn();
     const request = fetchJson(from()('http://example.com'));
     request.then(successSpy);
