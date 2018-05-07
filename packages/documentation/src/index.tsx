e789c3c6c572392a87c861bdba19c322c0462e80
Dean Merchant <deanmerchant@gmail.com>
2018-05-07 12:34:35 +0100

    Attempting to get docs working again

--- packages/documentation/src/index.tsx
+++ packages/documentation/src/index.tsx
@@ -1,8 +1,7 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import React from 'react';
+import { render } from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root') as HTMLElement);
+render(<App />, document.getElementById('root'));
 registerServiceWorker();
